571262aaa4a444d58d2e0b9fde2e05a8880c84ac
Nawazish Alam <nawazishmail@gmail.com>
2018-06-30 15:13:08 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,4 +1,4 @@
-host_temp =r"C:\Users\Nawa\Desktop\python\Simple_application\hosts"
+host_temp ="hosts"
 host_path =r"C:\Windows\System32\drivers\etc\hosts"
 redirect = "127.0.0.1"
 website_list = ['facebook.com','www.facebook.com']
